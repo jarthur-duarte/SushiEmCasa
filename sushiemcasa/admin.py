3d777d494a0b04fc30e2eb5f4ce568461b0440db
Luiza Barbosa <luizabalmeidass@gmail.com>
2025-09-27 20:25:03 -0300

    Criação do usuario admin

--- sushiemcasa/admin.py
+++ sushiemcasa/admin.py
@@ -16,3 +16,25 @@ class OrderAdmin(admin.ModelAdmin):
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ('order', 'item_name', 'quantity', 'price')
+
+
+
+
+from django.contrib import admin
+from sushiemcasa.models.contato import MensagemFeedback
+
+@admin.register(MensagemFeedback)
+class MensagemFeedbackAdmin(admin.ModelAdmin):
+    
+    # Colunas que serão exibidas na lista de feedbacks
+    list_display = ('nome', 'email', 'data_envio')
+
+    # Adiciona um filtro na lateral direita por data
+    list_filter = ('data_envio',)
+
+    # Adiciona uma barra de busca que procura nestes campos
+    search_fields = ('nome', 'email', 'mensagem')
+    
+    # Faz com que os campos não sejam editáveis no admin
+    # (geralmente não queremos editar o feedback de um usuário)
+    readonly_fields = ('nome', 'email', 'mensagem', 'data_envio')
