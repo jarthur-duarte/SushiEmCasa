b5914d473146c0467ded2ff4be94d6eb56b2a531
Luiza Barbosa <luizabalmeidass@gmail.com>
2025-09-27 20:23:56 -0300

    inserção da url do contato

--- sushiemcasa/urls.py
+++ sushiemcasa/urls.py
@@ -7,7 +7,7 @@ app_name = 'sushiemcasa'
 urlpatterns = [
     # A URL que você perguntou vai aqui
     path('', views.homepage, name='homepage'),
-    path('contato/', views.pagina_de_contato, name='contato'),
+    path('contato/', views.pagina_contato, name='contato'),
 
     # Você pode adicionar outras URLs deste app aqui no futuro
     # path('cardapio/', views.mostrar_cardapio, name='cardapio'),
