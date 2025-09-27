aa91d046fc2120a083141eff9abb973376dae239
Bernardo <bernardoguimaraesnp@gmail.com>
2025-09-27 12:08:30 -0300

    Registro de Modelos no admin.py

--- sushiemcasa/admin.py
+++ sushiemcasa/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from    .models import Order, OrderItem
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['order']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'status', 'created_at', 'total_price')
+    list_filter = ('status', 'created_at')
+    inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'item_name', 'quantity', 'price')
