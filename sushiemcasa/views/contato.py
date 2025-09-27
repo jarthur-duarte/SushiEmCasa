2ca66a30ab77c89116eee2822758da35d9957b51
jarthur-duarte <joaoarthurdduarte@gmail.com>
2025-09-27 10:15:55 -0300

    "implementado a aba contatos na main"

--- sushiemcasa/views/contato.py
+++ sushiemcasa/views/contato.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def pagina_de_contato(request):
     # sua lógica de contato aqui
-    return render(request, 'contato.html')
+    return render(request, 'sushiemcasa/contato.html')
