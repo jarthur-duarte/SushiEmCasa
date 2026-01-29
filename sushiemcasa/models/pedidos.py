f1371e907bee8880fd077b8a68bfbdda2a45e4e2
jarthur-duarte <joaoarthurdduarte@gmail.com>
2026-01-29 10:21:01 -0300

    Site pronto: Guest checkout e Timezone

--- sushiemcasa/models/pedidos.py
+++ sushiemcasa/models/pedidos.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 import datetime
 from django.core.exceptions import ValidationError
 
-
 class Order(models.Model):
     STATUS_CHOICES = (
         ('Pending', 'Pendente'),
@@ -14,17 +13,25 @@ class Order(models.Model):
         ('Cancelled', 'Cancelado'),
     )
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    
     created_at = models.DateTimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    
     delivery_datetime = models.DateTimeField(
         verbose_name="Delivery Date and Time",
         null=True,
         blank=True
     )
+
     def __str__(self):
-        return f"Order #{self.id} - {self.user.username}"
+        if self.user:
+            nome_cliente = self.user.username
+        else:
+            nome_cliente = "Convidado (Sem Login)"
+            
+        return f"Order #{self.id} - {nome_cliente}"
     
     def clean(self):
         if self.delivery_datetime:
@@ -39,7 +46,6 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     
-
     produto = models.ForeignKey(
         'sushiemcasa.Produto', 
         on_delete=models.SET_NULL, 
--- sushiemcasa/views/pedidos.py
+++ sushiemcasa/views/pedidos.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from sushiemcasa.models.pedidos import Order
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/usuario/login/') 
 def pagina_orders(request):
-    todos_os_pedidos = Order.objects.all().order_by('-created_at')
+    usuario = request.user
+    
+    if usuario.is_staff:
+        todos_os_pedidos = Order.objects.all().order_by('-created_at')
+    else:
+        todos_os_pedidos = Order.objects.filter(costumer=usuario).order_by('-created_at')
+
     context = {
         'pedidos': todos_os_pedidos
     }
-
+    
     return render(request, 'sushiemcasa/pedidos.html', context)
