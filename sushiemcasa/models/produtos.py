dc40a1b7154019a789439ba5d52b8158daf5c279
jarthur-duarte <joaoarthurdduarte@gmail.com>
2026-01-30 14:49:27 -0300

    "ajeitando mobile dev"

--- sushiemcasa/models/produtos.py
+++ sushiemcasa/models/produtos.py
@@ -1,7 +1,5 @@
-# sushiemcasa/models/produtos.py
 from django.db import models
 
-# NOVO MODELO: Categoria
 class Categoria(models.Model):
     """ Define uma categoria para filtrar o cardápio (ex: Temaki, Combinado). """
     nome = models.CharField(max_length=50, unique=True)
@@ -15,7 +13,6 @@ class Categoria(models.Model):
         verbose_name = "Categoria"
 
 
-# MODELO EXISTENTE: Produto
 class Produto(models.Model):
     """ Define a estrutura de um produto de sushi no cardápio. """
     disponivel = models.BooleanField(default=True)
@@ -31,11 +28,9 @@ class Produto(models.Model):
     imagem = models.ImageField(upload_to='produtos/') 
 
     def __str__(self):
-        # Agora mostra a categoria no Admin também
         return f"{self.nome} ({self.categoria.nome})" 
     
     class Meta:
         verbose_name_plural = "Produtos"
         verbose_name = "Produto"
 
-# Remova quaisquer classes Order ou OrderItem que possam ter sido duplicadas aqui!
